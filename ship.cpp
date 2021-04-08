1089e2712077fe79aaf322db8814c36fbd8e9feb
Jason <yunj@stud.ntnu.no>
2021-04-08 20:29:02 +0200

    made separate files for ship

--- ship.cpp
+++ ship.cpp
@@ -1,4 +1,29 @@
 //
 // Created by Jason Ji on 4/8/21.
 //
+#include <iostream>
+#include <string>
+#include "ship.h"
 
+void Ship :: printType(){
+    switch (shipType) {
+        case (Junk): cout << "Junk Boat"; break;
+        case (OneMastedSloop): cout << "One-Masted Sloop"; break;
+        case (TwoMastedSloop): cout << "Two-Masted Sloop"; break;
+        case (Brigantine): cout << "Brigantine"; break;
+        case (Juggernaut): cout << "Juggernaut"; break;
+    }
+}
+void Ship ::  setData(string nvn, Type t, int guns, float spd, int strength, int space){
+    name = nvn; shipType = t; numGuns = guns; speed = spd; hullStrength = strength; cargoSpace = space;
+}
+void Ship ::  printData(){
+    cout << "\t\"" << name << "\" "; printType(); cout << endl;
+    cout << "\t Speed: " << speed << " Guns: " << numGuns << endl;
+}
+void Ship :: printDetailData(){
+    cout << name << " " << shipType << " " << speed << " Guns: ";
+    cout << numGuns << " hull strength: " << hullStrength << " Cargospace: " << cargoSpace << endl;
+}
+
+vector <Ship*> gShip;
--- ship.h
+++ ship.h
@@ -1,8 +1,44 @@
 //
 // Created by Jason Ji on 4/8/21.
 //
+#include <string>
+
+using namespace std;
 
 #ifndef BOATSNHOES_SHIP_H
 #define BOATSNHOES_SHIP_H
+
+
+
+enum Type {Junk, OneMastedSloop, TwoMastedSloop, Brigantine, Juggernaut};
+
+class Ship {
+private:
+    string name;
+    Type shipType;
+    int numGuns;
+    float speed;
+    int hullStrength;
+    int cargoSpace;
+public:
+    Ship(){
+        name = "default";
+        shipType = Junk;
+        numGuns = 0;
+        speed = 2;
+        hullStrength = 4;
+        cargoSpace = 25;
+    }
+    void printType();
+    void setData(string nvn, Type t, int guns, float spd, int strength, int space);
+    void printData();
+    void printDetailData();
+
+};
+
+class PlayerShip : public Ship{
+    int usedCargo;
+};
+
 
 #endif //BOATSNHOES_SHIP_H
